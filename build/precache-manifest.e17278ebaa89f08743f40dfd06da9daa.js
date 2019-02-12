9065083b8949e2f272478fe06094f78670811008
Antoxa <770386@gmail.com>
2019-02-12 23:03:36 +0300

    исправлена ошибка в тексте описания

---
+++ build/precache-manifest.e17278ebaa89f08743f40dfd06da9daa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5eed80fe0ac4f05b43",
+    "url": "/static/css/main.06979a67.chunk.css"
+  },
+  {
+    "revision": "dc5eed80fe0ac4f05b43",
+    "url": "/static/js/main.dc5eed80.chunk.js"
+  },
+  {
+    "revision": "124e14f617d09d71bf05",
+    "url": "/static/css/1.7b91c620.chunk.css"
+  },
+  {
+    "revision": "124e14f617d09d71bf05",
+    "url": "/static/js/1.124e14f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02ade7703f4f4c4f1b74c0142ad1b9a3",
+    "url": "/static/media/overview_en.02ade770.md"
+  },
+  {
+    "revision": "1d699212f75958972a2ef16dbbcaa5f2",
+    "url": "/static/media/overview_ru.1d699212.md"
+  },
+  {
+    "revision": "4274c1bf85c0927913d3d66d5b4fd9a8",
+    "url": "/static/media/privatepolicy_en.4274c1bf.md"
+  },
+  {
+    "revision": "a957e745715b256c729c6d147041e113",
+    "url": "/static/media/privatepolicy_ru.a957e745.md"
+  },
+  {
+    "revision": "c35ce2e05c642ae1cef60b1c7f0ff135",
+    "url": "/index.html"
+  }
+];
