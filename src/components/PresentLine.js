7c2ef3b27455bff88e699f6d8d1041d4f099db01
Antoxa <770386@gmail.com>
2019-03-07 22:47:57 +0300

    добавление в описание как учитывается трафик в аккаунте при передаче; небольшие исправления в дизайне

--- src/components/PresentLine.js
+++ src/components/PresentLine.js
@@ -3,12 +3,12 @@ import React from 'react'
 //noinspection JSAnnotator
 export const PresentLine : React.StatelessComponent<{}> = props  => {
     return (
-        <div className="row justify-content-center align-items-center py-5">
+        <div className="row justify-content-center align-items-center py-3 py-lg-5">
             <div className="col-md-3 col-sm-4 col-6">
                 <img src={props.src} className="w-100 m-2" alt={props.alt}/>
             </div>
             <div className="col-xl-6 col-md-8 col-sm-8 col-12">
-                <p className="h1 text-center text-sm-left pl-2 pl-sm-5 pr-2 pt-5 pt-sm-0">
+                <p className="h1 text-center text-sm-left pl-2 pl-sm-5 pr-2 pt-3 pt-sm-0">
                     {props.children}
                 </p>
             </div>
